aa27b6c8f3161a98224f09e40c815d8fb74311bd
nuhptr <nugrahaadi733@gmail.com>
2021-02-22 14:55:31 +0700

    new commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bwa_cozy/pages/splash_page.dart';
+import 'package:bwa_cozy/provider/space_provider.dart';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => SpaceProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashPage(),
+      ),
+    );
+  }
+}
---
+++ lib/provider/space_provider.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:bwa_cozy/model/space.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SpaceProvider extends ChangeNotifier {
+  getRecomendedSpace() async {
+    var result =
+        await http.get('https://bwa-cozy.herokuapp.com/recommended-spaces');
+    print(result.statusCode);
+    print(result.body);
+
+    if (result.statusCode == 200) {
+      List data = jsonDecode(result.body);
+      List<Space> space = data.map((item) => Space.fromJson(item)).toList();
+      return space;
+    } else {
+      return <Space>[];
+    }
+  }
+}
